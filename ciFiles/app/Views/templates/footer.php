9884faba266e42eb35d453819e8ab8c1305048ba
Ratnesh KArbhari <ratneshkarbhari74@gmail.com>
2021-04-08 13:06:44 +0530

    saving changes

--- ciFiles/app/Views/templates/footer.php
+++ ciFiles/app/Views/templates/footer.php
@@ -2,25 +2,25 @@
         <div class="container">
             <form action="#" class="row" method="post">
                 <div class="col-lg-3 d-grid gap-2">
-                    <input class="d-block" type="text" placeholder="Name" name="full_name" id="full_name" style="padding: 2%; border-radius: 30px; width: 100%;">
+                    <input class="d-block" type="text" placeholder="Name" name="full_name" id="full_name" style="padding: 3% 0 3% 5%; border-radius: 30px; width: 100%;">
                 </div>
                 <div class="col-lg-3 d-grid gap-2">
-                    <input class="d-block" type="email" placeholder="Email" name="email" id="email" style="padding: 2%; border-radius: 30px; width: 100%;">
+                    <input class="d-block" type="email" placeholder="Email" name="email" id="email" style="padding: 3% 0 3% 5%; border-radius: 30px; width: 100%;">
                 </div>
                 <div class="col-lg-3 d-grid gap-2">
-                    <input class="d-block" type="text" placeholder="Mobile Number" name="email" id="email" style="padding: 2%; border-radius: 30px; width: 100%;">
+                    <input class="d-block" type="text" placeholder="Mobile Number" name="email" id="email" style="padding: 3% 0 3% 5%; border-radius: 30px; width: 100%;">
                 </div>
                 <div class="col-lg-3 mb-3 d-grid gap-2">
                     <button class="btn d-block btn-primary w-100">Submit</button>
                 </div>
             </form>
-            <a href="#" style="border-radius: 30px; border"  class="btn btn-outline-primary text-light">QUICK LINKS</a><br>
-            <div class="row">
+            <a href="#" style="border-radius: 30px; margin-top: 2%;"  class="btn btn-outline-primary text-light">QUICK LINKS</a><br>
+            <div class="row" style="margin: 3% 0;">
                 <div class="text-center col-4">
-                    <p style="margin: 5% 0; font-size: 25px;" class="text-light contact-info"><img src="<?php echo site_url("assets/images/call_gold_new.png"); ?>" style="width: 30px; height: 30px;">022 2650 6597</p>
+                    <p style=" margin-bottom:0; margin-top: 2%; font-size: 25px;" class="text-light contact-info"><img src="<?php echo site_url("assets/images/call_gold_new.svg"); ?>" style="width: 30px; margin-right: 3%; height: 30px;">022 2650 6597</p>
                 </div>
                 <div class="text-center col-5">
-                    <p style="margin: 5% 0; font-size: 25px;" class="text-light contact-info"><img src="<?php echo site_url("assets/images/mail_new_gold.svg"); ?>" style="width: 30px; height: 30px;">info@preciousgroup.com</p>
+                    <p style=" margin-bottom:0; margin-top: 2%; font-size: 25px;" class="text-light contact-info"><img src="<?php echo site_url("assets/images/mail_new_gold.svg"); ?>" style="width: 30px; margin-right: 3%; height: 30px;">info@preciousgroup.com</p>
                 </div>
                 <div class="text-center col-3">
                 <nav class="navbar navbar-expand-lg">
@@ -45,13 +45,11 @@
                 <div style="padding: 0; margin:0;" class="col-2">
                     <a class="btn d-block btn-primary w-100" >EMI CALCULATOR</a>
                 </div>
-                <div style="padding: 0; margin:0;" class="col-9">
+                <div style="padding: 0; margin:0;" class="col-10">
                     <hr style="background-color: white;">
                     <div class="text-right text-light">&copy; Precious Group. All Rights Reserved.</div>
                 </div>
-                <div style="padding: 0; margin:0;" class="col-1">
-                    <img src="<?php echo site_url("assets/images/Precious_ logo_2.png"); ?>" style="width: 100%;">
-                </div>
+                
             </div>
         </div>
     </footer>
